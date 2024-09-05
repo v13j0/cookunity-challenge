461379e8b5f2d2efaf4575271a29a485bf04a7a0
D <3704877+v13j0@users.noreply.github.com>
2024-09-05 18:23:50 -0300

    add dotenv config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
